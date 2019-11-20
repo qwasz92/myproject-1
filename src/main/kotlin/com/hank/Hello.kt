0135db8580ffe87dd0acf3783dda6c4cf72ba075
Hank Tom <lovevul35j6@gmail.com>
2019-11-20 13:28:14 +0800

    Java&Kotlin

--- src/main/kotlin/com/hank/Hello.kt
+++ src/main/kotlin/com/hank/Hello.kt
@@ -5,6 +5,11 @@ fun main() {
 //    Human().hello()
     val h=Human()
     h.hello()
+    var age:Int = 19
+        age=20
+    var weight = 66.5f
+    var name :String
+        name= "Hank"
 }
 class Human {
     fun hello (){
